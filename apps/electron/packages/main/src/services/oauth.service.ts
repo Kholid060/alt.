e7121b03749e648e55572cd5e819fcfb7691d7ce
Ahmad Kholid <kholid060@gmail.com>
2024-06-02 17:12:20 +0800

    feat(electron): add oauth2 refresh access token

--- apps/electron/packages/main/src/services/oauth.service.ts
+++ apps/electron/packages/main/src/services/oauth.service.ts
@@ -9,14 +9,20 @@ import { debugLog } from '#packages/common/utils/helper';
 import { CustomError } from '#packages/common/errors/custom-errors';
 import { z } from 'zod';
 import { fromZodError } from 'zod-validation-error';
-import type { ExtensionCredential } from '@repo/extension-core/src/client/manifest/manifest-credential';
 import { nanoid } from 'nanoid/non-secure';
 import EventEmitter from 'eventemitter3';
-import { shell } from 'electron';
+import { safeStorage, shell } from 'electron';
 import { logger } from '../lib/log';
+import { parseJSON } from '@repo/shared';
+import type { ExtensionCredential } from '@repo/extension-core/src/client/manifest/manifest-credential';
 
 const SERVER_TIMEOUT_MS = 300_000; // 5 Minutes
 const OAUTH_CALLBACK_PATHNAME = new URL(OAUTH_CALLBACK_URL).pathname;
+
+const Oauth2CredValueSchema = z.object({
+  clientId: z.string().min(1, { message: 'Missing clientId' }),
+  clientSecret: z.string().min(1, { message: 'Missing clientId' }),
+});
 
 interface OauthServiceServerEvents {
   'authorize-callback': (url: URL) => void;
@@ -94,6 +100,7 @@ interface OAuth2ProviderOptions {
   callbackURL: string;
   clientSecret: string;
   provider: ExtensionCredential;
+  resolver?: PromiseWithResolvers<void>;
   credential: { id: string; oauthTokenId: number | null };
 }
 class Oauth2Provider implements OAuth2ProviderOptions {
@@ -102,17 +109,20 @@ class Oauth2Provider implements OAuth2ProviderOptions {
   callbackURL: string;
   clientSecret: string;
   provider: ExtensionCredential;
+  resolver?: PromiseWithResolvers<void>;
   credential: { id: string; oauthTokenId: number | null };
 
   constructor({
     clientId,
+    resolver,
     provider,
     credential,
     callbackURL,
     clientSecret,
   }: OAuth2ProviderOptions) {
-    this.id = nanoid();
+    this.id = nanoid(8);
     this.clientId = clientId;
+    this.resolver = resolver;
     this.provider = provider;
     this.credential = credential;
     this.callbackURL = callbackURL;
@@ -158,12 +168,31 @@ class Oauth2Provider implements OAuth2ProviderOptions {
 
     return token;
   }
+
+  async refreshAccessToken(refreshToken: string) {
+    const searchParams = new URLSearchParams();
+    searchParams.set('grant_type', 'refresh_token');
+    searchParams.set('refresh_token', refreshToken);
+    searchParams.set('client_id', this.clientId);
+    searchParams.set('client_secret', this.clientSecret);
+
+    const response = await fetch(
+      `${this.provider.auth.tokenUrl}?${searchParams.toString()}`,
+      {
+        method: 'POST',
+      },
+    );
+    if (!response.ok) {
+      throw new Error(`[${response.status}] ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    const token = await Oauth2ResponseSchema.parseAsync(result);
+
+    return token;
+  }
 }
 
-const Oauth2CredValueSchema = z.object({
-  clientId: z.string().min(1, { message: 'Missing clientId' }),
-  clientSecret: z.string().min(1, { message: 'Missing clientId' }),
-});
 class OauthService {
   static instance = new OauthService();
 
@@ -177,12 +206,16 @@ class OauthService {
   private initServer() {
     const server = new OauthServiceServer();
     server.addListener('authorize-callback', async (url) => {
+      let resolver: undefined | PromiseWithResolvers<void>;
+
       try {
         const code = url.searchParams.get('code');
         const state = url.searchParams.get('state');
         const authProvider = state && this.authSessions.get(state);
 
         if (!code || !authProvider) return;
+
+        resolver = authProvider.resolver;
 
         const token = await authProvider.exchangeCode(code);
         const payload = {
@@ -203,19 +236,23 @@ class OauthService {
             payload,
           );
         }
+
+        resolver?.resolve();
       } catch (error) {
         logger(
           'error',
           ['OAuthService', 'authorize-callback'],
           (<Error>error).message,
         );
+
+        resolver?.reject(error);
       }
     });
 
     this.server = server;
   }
 
-  async startAuth(credentialId: string) {
+  async startAuth(credentialId: string, waitAuth?: boolean) {
     const credential =
       await DBService.instance.extension.getCredentialValueDetail(credentialId);
     if (!credential) {
@@ -242,9 +279,11 @@ class OauthService {
       throw new CustomError(fromZodError(credentialValue.error).message);
     }
 
+    const resolver = waitAuth ? Promise.withResolvers<void>() : undefined;
     const oauth2Provider = new Oauth2Provider({
       ...credentialValue.data,
       provider,
+      resolver,
       callbackURL: OAUTH_CALLBACK_URL,
       credential: {
         id: credential.id,
@@ -256,6 +295,90 @@ class OauthService {
 
     this.server.start();
     oauth2Provider.startAuth();
+
+    return resolver?.promise;
+  }
+
+  async refreshAccessToken(extensionId: string, providerId: string) {
+    const credential =
+      await DBService.instance.db.query.extensionCreds.findFirst({
+        columns: {
+          id: true,
+          type: true,
+          value: true,
+        },
+        with: {
+          extension: {
+            columns: {
+              credentials: true,
+            },
+          },
+          oauthToken: {
+            columns: {
+              // it throws "JSON cannot hold BLOB values" error
+              // refreshToken: true,
+              id: true,
+            },
+          },
+        },
+        where(fields, operators) {
+          return operators.and(
+            operators.eq(fields.providerId, providerId),
+            operators.eq(fields.extensionId, extensionId),
+          );
+        },
+      });
+    const provider = credential?.extension?.credentials?.find(
+      (item) => item.providerId === providerId,
+    );
+    if (!provider || !credential || !credential.oauthToken?.id) {
+      throw new Error("Couldn't find the extension credential");
+    }
+
+    if (!provider.auth.tokenUrl) {
+      throw new Error(
+        'The credential provider doesn\'t have "tokenUrl" in its manifest',
+      );
+    }
+
+    const oauthToken =
+      await DBService.instance.extension.getCredentialOAuthToken(
+        credential.oauthToken.id,
+      );
+    if (!oauthToken) throw new Error("This app hasn't been connected");
+    if (!oauthToken.refreshToken) {
+      throw new Error("This app doesn't have refresh token");
+    }
+
+    const credentialValue = await Oauth2CredValueSchema.safeParseAsync(
+      parseJSON(safeStorage.decryptString(credential.value), {}),
+    );
+    if (!credentialValue.success) {
+      throw new CustomError(fromZodError(credentialValue.error).message);
+    }
+
+    const oauth2Provider = new Oauth2Provider({
+      callbackURL: OAUTH_CALLBACK_URL,
+      credential: {
+        id: credential.id,
+        oauthTokenId: oauthToken.id,
+      },
+      provider,
+      ...credentialValue.data,
+    });
+    const token = await oauth2Provider.refreshAccessToken(
+      oauthToken.refreshToken,
+    );
+
+    await DBService.instance.extension.updateCredentialOauthToken({
+      scope: token.scope,
+      tokenType: token.token_type,
+      accessToken: token.access_token,
+      refreshToken: token.refresh_token,
+      expiresTimestamp: Date.now() + token.expires_in * 1000,
+    });
+
+    return token;
   }
 }
 
