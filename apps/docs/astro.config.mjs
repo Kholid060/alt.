21759abf2a6e25903feb851efbdf1ca1681fcd76
Ahmad Kholid <kholid060@gmail.com>
2024-08-15 14:15:33 +0800

    feat(docs): add workflow variables page

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -2,39 +2,45 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightImageZoom from 'starlight-image-zoom';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	redirects: {
-		'/': '/basics/introduction'
-	},
-	integrations: [
-		starlight({
-			title: 'Alt. Docs',
-			logo: {
-				src: './src/assets/logo.svg'
-			},
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Basics',
-					autogenerate: { directory: 'basics' },
-				},
-				{
-					label: 'Extensions',
-					autogenerate: { directory: 'extensions' },
-				},
-				{
-					label: 'Workflows',
-					autogenerate: { directory: 'workflows' },
-				},
-				{
-					label: 'API Reference',
-					autogenerate: { directory: 'api-reference' },
-				},
-			],
-			plugins: [starlightImageZoom()],
-		}),
-	],
+  redirects: {
+    '/': '/basics/introduction'
+  },
+  integrations: [starlight({
+    title: 'Alt. Docs',
+    logo: {
+      src: './src/assets/logo.svg'
+    },
+		customCss: [
+			'./src/assets/css/style.css'
+		],
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Basics',
+      autogenerate: {
+        directory: 'basics'
+      }
+    }, {
+      label: 'Extensions',
+      autogenerate: {
+        directory: 'extensions'
+      }
+    }, {
+      label: 'Workflows',
+      autogenerate: {
+        directory: 'workflows'
+      }
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }],
+    plugins: [starlightImageZoom()]
+  }), react()]
 });
