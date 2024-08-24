4e0575bad6c79b2174b004d22be99c145d7e1cc1
Ahmad Kholid <kholid060@gmail.com>
2024-08-24 13:19:23 +0800

    fix(web): router devtools on production

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -9,7 +9,16 @@ import {
   Outlet,
   redirect,
 } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { lazy } from 'react';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : lazy(() =>
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      );
 
 export const Route = createRootRouteWithContext<AppRouterContext>()({
   component: AppRoot,
