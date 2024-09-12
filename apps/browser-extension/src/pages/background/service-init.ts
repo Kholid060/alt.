64489e4e715a673431a0d2d87be0793b1145773a
Ahmad Kholid <kholid060@gmail.com>
2024-09-12 17:40:51 +0800

    fix(browser-extension): error when clearing badge

--- apps/browser-extension/src/pages/background/service-init.ts
+++ apps/browser-extension/src/pages/background/service-init.ts
@@ -2,6 +2,7 @@ import BrowserService from '@root/src/service/browser.service';
 import WebsocketService from '@root/src/service/websocket.service';
 import Browser from 'webextension-polyfill';
 import BackgroundBrowserEventListener from './BackgroundBrowserEventListener';
+import { IS_FIREFOX } from '@root/src/utils/constant/constant';
 
 function initWebsocketService() {
   WebsocketService.instance.addListener('connect', async () => {
@@ -12,7 +13,9 @@ function initWebsocketService() {
   WebsocketService.instance.addListener('disconnect', async () => {
     BrowserService.instance.stopListener();
     await Browser.action.setBadgeText({ text: '' });
-    await Browser.action.setBadgeBackgroundColor({ color: null });
+    await Browser.action.setBadgeBackgroundColor({
+      color: IS_FIREFOX ? null : '',
+    });
   });
 
   WebsocketService.instance.init();
